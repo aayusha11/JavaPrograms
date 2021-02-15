a4a2dcda9512861298115f8c3abdbd530d606c3d
aayusha11 <t-aayusha.srivastava@ocltp.com>
2021-02-15 22:14:29 +0530

    rest folder removed

--- rest/dao/CourseDao.java
+++
@@ -1,9 +0,0 @@
-package com.springbootrest.rest.dao;
-
-import com.springbootrest.rest.entities.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CourseDao extends JpaRepository<Course, Integer> {
-}
